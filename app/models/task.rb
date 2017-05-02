86dd883849b325a8e99fb93f1ee124220d6417eb
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-05-01 20:00:23 -0600

    fix double problem

--- app/models/task.rb
+++ app/models/task.rb
@@ -46,4 +46,10 @@ class Task
                       WHERE id = ?;", id)
   end
 
+  def self.database
+    database = SQLite3::Database.new('db/task_manager_development.db')
+    database.results_as_hash = true
+    database
+  end
+
 end
